7bf5ea91ce88969803099b97bc7fe66b21aa4841
Haidar <haidaraliaalruwayj@mail.adelphi.edu>
2021-03-30 12:03:16 +0300

    done

---
+++ BasicCalculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter The First Num: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter The Operatore: ");
+            string op = Console.ReadLine();
+            if (op == "+")
+            {
+                Console.WriteLine("Enter the Second Num: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(a + b);
+            }
+            else if (op == "-")
+            {
+                Console.WriteLine("Enter the Second Num: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(a - b);
+            }
+            else if (op == "*")
+            {
+                Console.WriteLine("Enter the Second Num : ");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(a * b);
+            }
+            else if (op == "/")
+            {
+                Console.WriteLine("Enter the Second Num : ");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine(a / b);
+            }
+            else
+            {
+                Console.WriteLine("Enter a Valid Operatore! Which are +, -, * or /");
+            }
+
+        }
+    }
+
+}
